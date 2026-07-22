f8567898cab9539c413d268ee9bb1963d173382f
TheEEPYLynx <eepylynx32@gmail.com>
2026-07-22 05:31:51 -0600

    Connected archive interface to PBS database

--- app.js
+++ app.js
@@ -1,29 +1,91 @@
+let database = null;
+
+fetch("database/database.json")
+    .then(response => response.json())
+    .then(data => {
+        database = data;
+        console.log("PBS Database Loaded");
+    });
+
+
 function openCategory(category) {
 
     let output = document.getElementById("output");
 
+    if (!database) {
+        output.innerHTML = "<p>Database loading...</p>";
+        return;
+    }
+
+
     if (category === "Factions") {
-        output.innerHTML =
-        "<h3>Factions</h3>" +
-        "<p>Database entries will be added here.</p>";
+
+        output.innerHTML = "<h3>Factions</h3>";
+
+        database.factions.forEach(item => {
+
+            output.innerHTML +=
+            `
+            <p>
+            <b>${item.name}</b> (${item.short})<br>
+            ${item.description}
+            </p>
+            `;
+
+        });
+
     }
 
-    else if (category === "Technology") {
-        output.innerHTML =
-        "<h3>Technology</h3>" +
-        "<p>Technology archive currently under construction.</p>";
+
+    if (category === "Weapons") {
+
+        output.innerHTML = "<h3>Weapons</h3>";
+
+        database.weapons.forEach(item => {
+
+            output.innerHTML +=
+            `
+            <p>
+            <b>${item.name}</b><br>
+            ${item.description}
+            </p>
+            `;
+
+        });
+
     }
 
-    else if (category === "Weapons") {
-        output.innerHTML =
-        "<h3>Weapons</h3>" +
-        "<p>Weapon database currently under construction.</p>";
+
+    if (category === "Technology") {
+
+        output.innerHTML = "<h3>Technology</h3>";
+
+        database.technology.forEach(item => {
+
+            output.innerHTML +=
+            `
+            <p>
+            <b>${item.name}</b><br>
+            ${item.description}
+            </p>
+            `;
+
+        });
+
     }
 
-    else if (category === "HMCM") {
+
+    if (category === "HMCM") {
+
         output.innerHTML =
-        "<h3>HMCM</h3>" +
-        "<p>Heavy Military Combat Machine archive loading.</p>";
+        `
+        <h3>HMCM Archive</h3>
+        <p>
+        Heavy Military Combat Machine database.
+        Entries pending.
+        </p>
+        `;
+
     }
 
 }
